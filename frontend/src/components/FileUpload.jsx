616bc969815c1546a9b4fb8d0cd7211cc983d099
mhnash <nasir.shuvo@tuni.fi>
2024-09-27 02:58:11 +0300

    added deployed backend api link

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
+const socket = io('https://llm-pdf-summary.onrender.com', {
   reconnection: true,
   reconnectionDelay: 500,
   reconnectionAttempts: 10,
